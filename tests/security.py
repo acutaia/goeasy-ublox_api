366a20bd3affe81d0a20c7acb4c788fe012ba863
acutaia <angeloxx92@hotmail.it>
2021-03-17 11:16:37 +0100

    Add a private and a public key to generate valid tokens used for testing purpose

--- tests/security.py
+++ tests/security.py
@@ -24,13 +24,13 @@ Security Utilities for testing
 """
 
 # Standard library
+from datetime import datetime, timedelta
 import os
+import uuid
 
 # Third Party
 import dotenv
-import ujson
-import requests
-from pydantic import BaseModel
+from jose import jwt
 
 # ------------------------------------------------------------------------------
 
@@ -48,27 +48,16 @@ __docformat__ = "restructuredtext en"
 
 INVALID_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJUTU9ZZ1dhRUJxNVo4UWRSVmpZUnFUWEQyX0pxeHlTaWNGQjZGVFI3bXhzIn0.eyJqdGkiOiJlY2NjMzhmOS1kZmRjLTQ2NzUtODYzNi0wYzAyMjExODk1ZDQiLCJleHAiOjE2MDYzNDA0NTksIm5iZiI6MCwiaWF0IjoxNjA2MzQwMTU5LCJpc3MiOiJodHRwczovL2dhbGlsZW9jbG91ZC5nb2Vhc3lwcm9qZWN0LmV1L2F1dGgvcmVhbG1zL0dPRUFTWSIsImF1ZCI6ImRldmVsb3BlciIsInN1YiI6ImU3N2Q4NWFmLWQ0ZjAtNGEyOS04Yzc2LTU2NDYwOTk5ODI0YiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImRldmVsb3BlciIsImF1dGhfdGltZSI6MCwic2Vzc2lvbl9zdGF0ZSI6ImMzNzk5ZjY4LTUyZGItNDY0ZS05ODdlLTNiMTRjNjQzMjdiNyIsImFjciI6IjEiLCJjbGllbnRfc2Vzc2lvbiI6ImJhNGE0ODc5LWVjZmQtNGE0YS05YTI1LTdlYmIwZDk5OGMwNyIsImFsbG93ZWQtb3JpZ2lucyI6W10sInJlc291cmNlX2FjY2VzcyI6eyJkZXZlbG9wZXIiOnsicm9sZXMiOlsiVGVzdCJdfX0sIm5hbWUiOiIiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJkZXZlbG9wZXIifQ.AH7zxdSQwrNGxHqNTqkrPDlBD5TOf67C89M6HSDP8gievCb5woHzdPn3WYXvYrQdQhXPsh_Zfa0qqQiaVUnxX7z94NqExnZxleiRIeGm5XwXwxDm-aC8UiWW4TduGwAVyQ-xa9VxtEP_MfdW0KhOyOTowF93KRonQYRYVazQX4OZdcXsC-sIaJOFsAJPVqfxOSs7CNvFG1Xwid0Uv5nVPO916zITBYNb-qXDE38yaXd_jLmkq3jHb9KJFvf1VbPHbbYbtvNHAunohjyproo4BIAEnqdBw4AhVRtMo_Rt2mteXwoS92aMrNm8FsPVVbHB73IfsgSVO9I_NpYq-kqjgw"
 
-URL = "https://galileocloud.goeasyproject.eu/auth/realms/GOEASY/protocol/openid-connect/token"
+PATH = os.path.abspath(os.path.dirname(__file__))
 
-CLIENT_ID = "developer"
+with open(f"{PATH}/private.pem", "r") as fp:
+    PRIVATE_KEY = fp.read()
 
-USERNAME = "developer"
+with open(f"{PATH}/public.pem", "r") as fp:
+    PUBLIC_KEY = fp.read()
 
-PASSWORD = "password"
-
-GRANT_TYPE = "password"
-
-DATA = {
-    "client_id": CLIENT_ID,
-    "username": USERNAME,
-    "password": PASSWORD,
-    "grant_type": GRANT_TYPE
-}
-
-
-class Token(BaseModel):
-    """Token obtained from Keycloak"""
-    access_token: str
+ISSUER = "Travis-ci/test"
+AUDIENCE = "Travis-ci/test"
 
 # ------------------------------------------------------------------------------
 
@@ -78,13 +67,29 @@ def configure_security_for_testing():
     env_file = dotenv.find_dotenv()
     dotenv.load_dotenv(env_file)
     os.environ["REALM_ACCESS"] = '["Test"]'
-    os.environ["AUDIENCE"] = "developer"
+    os.environ["AUDIENCE"] = AUDIENCE
+    os.environ["ISSUER"] = ISSUER
+    os.environ["REALM_PUBLIC_KEY"] = PUBLIC_KEY
 
 
 def get_valid_token() -> str:
-    """Get a valid token for testing from Keycloak"""
-    r = requests.post(url=URL, data=DATA)
+    """
+    Generate a valid token using the private key associated to the public one\n
+    both keys are used only for testing purpose
+    """
+    to_encode = {
+        "jti": str(uuid.uuid4()),
+        "exp": datetime.utcnow() + timedelta(seconds=300),
+        "iat": datetime.utcnow(),
+        "iss": ISSUER,
+        "aud": AUDIENCE,
+        "realm_access": {
+            "roles": [
+                "Test"
+            ]
+        }
+    }
 
-    return Token.parse_obj(ujson.loads(r.content)).access_token
+    return jwt.encode(to_encode, PRIVATE_KEY, algorithm="RS256")
 
 
