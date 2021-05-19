e241bb9f96372423f03c0f1f0a5adeac6bd1f464
acutaia <angeloxx92@hotmail.it>
2021-05-19 13:22:30 +0200

    Fix format

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -2,18 +2,18 @@
 Test TLRUCache
 
 :author: Angelo Cutaia
-:copyright: Copyright 2021, Angelo Cutaia
+:copyright: Copyright 2021, LINKS Foundation
 :version: 1.0.0
 
 ..
 
-    Copyright 2021 Angelo Cutaia
+    Copyright 2021 LINKS Foundation
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,6 +21,7 @@ Test TLRUCache
     See the License for the specific language governing permissions and
     limitations under the License.
 """
+
 
 # Standard Library
 import time
